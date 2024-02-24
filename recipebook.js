4b0f0b79251c9c516ca7537252be71f31da92745
CelesteC43 <137976499+CelesteC43@users.noreply.github.com>
2024-02-23 21:01:23 -0700

    Finished Notification panel placeholder implementation

--- recipebook.js
+++ recipebook.js
@@ -1,3 +1,16 @@
+function newNotification(user="John", recipe="Yummy Recipe", time="15 min"){
+    newN = document.createElement("div");
+    newN.innerHTML = `<div class="alert alert-light" role="alert">
+                        <strong>${user}</strong> added a recipe: <strong>${recipe}</strong> 
+                        ${time} ago
+                      </div>`;
+    return newN;
+
+}
+
 setInterval(()=>{
-    
-},1000);
+    const newNot = newNotification("Anya","Sourdough","1 hour");
+
+    const notificationsContainer = document.getElementById('notifications-list');
+    notificationsContainer.innerHTML = newNot.innerHTML + notificationsContainer.innerHTML;
+},3000);
