d865f25d72e9a6599b8aa4d396168b241f5aaf22
CelesteC43 <137976499+CelesteC43@users.noreply.github.com>
2024-03-09 16:36:32 -0700

    anohter thing

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
-// const port = process.argv.length > 2 ? process.argv[2] : 4000;
- const port = 4000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+//  const port = 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
