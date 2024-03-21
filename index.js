0e3f53313bde6d55fbfce9ff92318a3c3a3057ae
CelesteC43 <137976499+CelesteC43@users.noreply.github.com>
2024-03-20 21:10:37 -0600

    outline for new endpoints and basic functions in index.js

--- index.js
+++ index.js
@@ -26,10 +26,20 @@ app.set('trust proxy', true);
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-apiRouter.get("/celeste",(req,res,next)=>{
-  res.send({message: "Hello World"})
-})
+//create user
 
+//login
+
+//logout
+
+//getUser
+
+
+
+//secure api router
+  //create
+  //use
+  //check authentication
 
 // Get Recipes
 apiRouter.get('/recipes', (_req, res) => {
@@ -38,16 +48,33 @@ apiRouter.get('/recipes', (_req, res) => {
   
 // New Recipe
 apiRouter.post('/newrecipe', (req, res) => {
-  recipes.push(req.body); ////convert between types??? is req.body in json?
+  recipes.push(req.body); 
   res.send(recipes);
 });
 
 
 
+
+
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) { //res starts empty? authtoken came from call to DB.getUser()
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
