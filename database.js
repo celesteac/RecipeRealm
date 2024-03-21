e660690d8fc9a9329fc356ade7779da1b4868171
CelesteC43 <137976499+CelesteC43@users.noreply.github.com>
2024-03-20 20:30:41 -0600

    create collections

--- database.js
+++ database.js
@@ -1,16 +1,19 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('rental');
+const db = client.db('happyrecipes');
+const userCollection = db.collection('users');  
+const recipesCollection = db.collection('recipes');
 
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
   console.log(`yay`);
-  
 })().catch((ex) => {
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
