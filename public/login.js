e37844114139f0385704a1509251446408aa2e94
CelesteC43 <137976499+CelesteC43@users.noreply.github.com>
2024-03-22 12:48:18 -0600

    login page changes based on logn status

--- public/login.js
+++ public/login.js
@@ -1,6 +1,36 @@
-function login(){
+(async () => {
+    const userName = localStorage.getItem('user'); //if already logged in
+    if (userName) { 
+      document.querySelector('#username').textContent = userName;
+      setDisplay('loginDialouge', 'none');
+      setDisplay('authenticatedDialouge', 'block');
+    } else {
+      setDisplay('loginDialouge', 'block');
+      setDisplay('authenticatedDialouge', 'none');
+    }
+})();
+
+
+
+function loginUser(){
+    loginOrCreate();
+}
+
+function createUser(){
+    loginOrCreate();
+}
+
+function loginOrCreate(){
     const nameEl = document.querySelector("#name");
     const passEl = document.querySelector("#password");
     localStorage.setItem("user",nameEl.value);
     localStorage.setItem("password",passEl.value);
 }
+
+//used to modify HTML in index.html
+function setDisplay(controlId, display) {
+    const playControlEl = document.querySelector(`#${controlId}`);
+    if (playControlEl) {
+      playControlEl.style.display = display;
+    }
+  }
