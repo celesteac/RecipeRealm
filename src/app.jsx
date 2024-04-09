40d34220684fbe7f30acc97635f8453dc3f4949b
CelesteC43 <137976499+CelesteC43@users.noreply.github.com>
2024-04-09 12:36:18 -0600

    things render!

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+  
+<div className="body ">
+  <header >
+      <nav className="navbar navbar-expand-sm text-bg-info border-bottom border-secondary border-5">
+          <span style={{fontSize: "30px"}} className="navbar-brand ms-4">Recipe Realm</span>
+          <div className="container-fluid">
+              <ul className="navbar-nav">
+                  <li className="nav-item"><a className="nav-link active" href="index.html">Login</a></li>
+                  <li className="nav-item" id="authenticatedItem1" style={{display: "none"}}><a className="nav-link" href="recipebook.html">Recipe Book</a></li>
+                  <li className="nav-item" id="authenticatedItem2" style={{display: "none"}}><a className="nav-link" href="addrecipe.html">Add a Recipe</a></li>
+              </ul>
+          </div>
+          <div className="pe-5">
+              <strong>User:</strong>
+              <span id="userdisplay"></span>
+          </div>
+      </nav>
+  </header>
+
+  <main className="container-fluid text-center" id="main">
+    <p>App here</p>
+
+  </main>
+
+  <footer className="bg-info">
+      <p>Celeste Chamberlin <a href="https://github.com/celesteac/Startup">Github repository</a></p>
+  </footer> 
+  
+</div>
+);
 }
